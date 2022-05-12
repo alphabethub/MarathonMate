912b095c27634911c50185d3b93fd28ebe1288e0
mmornn <bharath.chandrasekhar@siemens.com>
2022-05-12 22:28:13 +0530

    Updates

---
+++ precache-manifest.60de4ca124623299996bbc169adae2ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "62b4b46a88ec5a91d4e5",
+    "url": "/marathonmate/static/css/main.c8f96c62.chunk.css"
+  },
+  {
+    "revision": "62b4b46a88ec5a91d4e5",
+    "url": "/marathonmate/static/js/main.62b4b46a.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/marathonmate/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "368ec79d416589c54032",
+    "url": "/marathonmate/static/js/2.368ec79d.chunk.js"
+  },
+  {
+    "revision": "fad0349476f670a2ec18",
+    "url": "/marathonmate/static/js/runtime~main.fad03494.js"
+  },
+  {
+    "revision": "a25d0d141be5a1a6d1e2404a95c147c0",
+    "url": "/marathonmate/static/media/Ding.a25d0d14.m4a"
+  },
+  {
+    "revision": "15660f928626de92aef00546e1b9e760",
+    "url": "/marathonmate/static/media/logo.15660f92.jpg"
+  },
+  {
+    "revision": "a673d8153d5c75753eced8c4a468a901",
+    "url": "/marathonmate/index.html"
+  }
+];
